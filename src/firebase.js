5b90d6e2365af90c283803589efe0ad74b3c46a6
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-25 13:49:32 +0530

    Added firebase to the project with firestore

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcy8GehEL96rUuDLWXXKRyUZ_ED8_1uR8",
+  authDomain: "feedback-form-builder-8ece9.firebaseapp.com",
+  projectId: "feedback-form-builder-8ece9",
+  storageBucket: "feedback-form-builder-8ece9.appspot.com",
+  messagingSenderId: "58804313591",
+  appId: "1:58804313591:web:0300d8b202abef0bba22f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
