46ffb77f9a17a703829bb03ce4d31a52c1517a80
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-27 19:42:42 +0530

    added fields icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,11 @@
 import logo from './logo.png'
 import survey_logo from './survey_logo.png'
+import textarea_icon from './textarea_icon.svg'
+import star_icon from './star_icon.svg'
+import smiley_icon from './smiley_icon.svg'
+import radio_icon from './radio_icon.svg'
+import numerical_icon from './numerical_icon.svg'
+import input_icon from './input_icon.svg'
+import cate_icon from './cate_icon.svg'
 
-export{logo, survey_logo};
+export{logo, survey_logo, textarea_icon, star_icon, smiley_icon, radio_icon, numerical_icon, input_icon, cate_icon};
