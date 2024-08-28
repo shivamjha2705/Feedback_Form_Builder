4aeed6718fd7053f866fd16bb903ac54bce53486
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-28 09:51:04 +0530

    added smile emojis svg

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,5 +7,11 @@ import radio_icon from './radio_icon.svg'
 import numerical_icon from './numerical_icon.svg'
 import input_icon from './input_icon.svg'
 import cate_icon from './cate_icon.svg'
+import smile1 from './smile1.svg'
+import smile2 from './smile2.svg'
+import smile3 from './smile3.svg'
+import smile4 from './smile4.svg'
+import smile5 from './smile5.svg'
 
-export{logo, survey_logo, textarea_icon, star_icon, smiley_icon, radio_icon, numerical_icon, input_icon, cate_icon};
+
+export { logo, survey_logo, textarea_icon, star_icon, smiley_icon, radio_icon, numerical_icon, input_icon, cate_icon, smile1, smile2, smile3, smile4, smile5 };
